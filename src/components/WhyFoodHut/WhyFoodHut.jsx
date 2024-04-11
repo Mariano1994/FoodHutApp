cbb38015df24e4a2ab9490981cd439480c5a8bd4
Mariano Capiliku <mariano@Marianos-MBP.mshome.net>
2024-04-11 18:03:45 +0100

    Finished whyFoodHutComponent

--- src/components/WhyFoodHut/WhyFoodHut.jsx
+++ src/components/WhyFoodHut/WhyFoodHut.jsx
@@ -1,5 +1,8 @@
 import './WhyFoodHut.css'
 import Boss from "../../assets/boss1.png"
+import hourIcon from '../../assets/24h-icon.svg'
+import OnlineIcon from '../../assets/online-icon.svg'
+import CalendarIcon from '../../assets/calendar-icon.svg'
 
 export const WhyFoodHut = () => {
   return (
@@ -17,37 +20,41 @@ export const WhyFoodHut = () => {
             <p>This is a type of resturent which typically serves food and drink, in addition to light refreshments such as baked goods or snacks. The term comes frome the rench word meaning food</p>
           </div>
           <div className='why-foodhut__services'>
-            <div className='why-foodhut__service'>
-              <span>icon</span>
-              <span>Online Order</span>
+            <div className='services-left'>
+                <div className='why-foodhut__service'>
+                  <img src={OnlineIcon} />
+                  <span>Online Order</span>
+                </div>
+
+                <div className='why-foodhut__service'>
+                  <img src={CalendarIcon} />
+                  <span>Pre-Reservation</span>
+                </div>
+
+                <div className='why-foodhut__service'>
+                  <img src={CalendarIcon}/>
+                  <span>Super Chef</span>
+                </div>
             </div>
 
-            <div className='why-foodhut__service'>
-              <span>icon</span>
-              <span>Pre-Reservation</span>
-            </div>
+            <div className='services-right'>
+              <div className='why-foodhut__service'>
+                  <img src={hourIcon}/>
+                  <span>24/7 Service</span>
+                </div>
 
-            <div className='why-foodhut__service'>
-              <span>icon</span>
-              <span>Super Chef</span>
-            </div>
+              <div className='why-foodhut__service'>
+              <img src={CalendarIcon}/>
+                <span>Organized FoodHut Palce</span>
+              </div>
 
-            <div className='why-foodhut__service'>
-              <span>icon</span>
-              <span>24/7 Service</span>
-            </div>
-
-            <div className='why-foodhut__service'>
-              <span>icon</span>
-              <span>Organized FoodHut Palce</span>
-            </div>
-
-            <div className='why-foodhut__service'>
-              <span>icon</span>
-              <span>Claen Kitchen</span>
-            </div>
+              <div className='why-foodhut__service'>
+              <img src={CalendarIcon}/>
+                <span>Claen Kitchen</span>
+              </div>
           </div>
-          <a href="#"> About Us</a>
+        </div>
+          <a href="#" className='about-us'> About Us</a>
         </div>
       </div>
     </>
