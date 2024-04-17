d4cb18128f5b1789e7045d9d56055ab03febb2bb
Mariano Capiliku <marianocapiliku@gmail.com>
2024-04-17 12:27:25 +0100

    Fecthing data from API using useEffect Hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 import { Footer } from "./components/Footer/Footer";
 import { HeroSection } from "./components/HeroSection/HeroSetion";
@@ -11,6 +11,18 @@ import { CardObjData } from "./utils/data";
 export const App = () => {
   const [foodCards, setFoodCards] = useState(CardObjData);
   const [query, setQuery] = useState("");
+
+  useEffect(() => {
+    async function fetchData() {
+      const data = await fetch(
+        "https://www.swiggy.com/dapi/restaurants/list/v5?lat=17.406498&lng=78.47724389999999&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+      );
+      const json = await data.json();
+      console.log(json);
+    }
+
+    fetchData();
+  }, []);
 
   const handlerSetQuery = (event) => {
     setQuery(event.target.value);
